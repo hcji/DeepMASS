b0dbd63379d49059f037c597425f4687050a84cb
hcji <ji.hongchao@foxmail.com>
2019-01-02 17:38:40 +0800

    save model

--- test.py
+++ test.py
@@ -6,6 +6,6 @@ Created on Tue Oct  9 14:09:55 2018
 """
 
 from DeepMASS import build_model, fine_tune, leave_one_out_test
-build_model(Test=True, Save=False)
-fine_tune(Test=True, Save=False)
+build_model(Test=True, Save=True)
+fine_tune(Test=True, Save=True)
 leave_one_out_test(energy='40V', database='structureDB')
